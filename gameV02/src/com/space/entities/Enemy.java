5913e231f5afd95f8030038a3065aa93c19a3e76
rafael <rafael.cerqueira@afgbrasil.com.br>
2023-03-07 14:07:37 -0300

    Animando e incluindo colisão nos inimigos

--- gameV02/src/com/space/entities/Enemy.java
+++ gameV02/src/com/space/entities/Enemy.java
@@ -2,10 +2,29 @@ package com.space.entities;
 
 import java.awt.image.BufferedImage;
 
+import com.space.main.Game;
+import com.space.world.World;
+
 public class Enemy extends Entity {
+	
+	private double speed = 1;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 	}
 
+	public void tick() {
+		if ((int)x < Game.player.getX() && World.isFree((int)(x+speed), this.getY())) {
+			x+=speed;
+		} else if ((int)x > Game.player.getX() && World.isFree((int)(x-speed), this.getY())) {
+			x-=speed;
+		}
+		
+		// colocar um else para inimigo andar uma direção por vez
+		if ((int)y < Game.player.getY() && World.isFree(this.getX(), (int)(y+speed))) {
+			y+=speed;
+		} else if ((int)y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed))) {
+			y-=speed;
+		}
+	}
 }
