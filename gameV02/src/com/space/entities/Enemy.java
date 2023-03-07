8b09b248828a17445b83bfecb2770b55b4313cb1
rafael <rafael.cerqueira@afgbrasil.com.br>
2023-03-07 15:25:07 -0300

    Ajustes na animação dos inimigos

--- gameV02/src/com/space/entities/Enemy.java
+++ gameV02/src/com/space/entities/Enemy.java
@@ -1,17 +1,27 @@
 package com.space.entities;
 
+import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import com.space.main.Game;
+import com.space.world.Camera;
 import com.space.world.World;
 
 public class Enemy extends Entity {
 	
-	private double speed = 1;
+	private double speed = 0.4;
+	
+	private int maskx = 8, masky = 8, maskw = 10, maskh = 10;
+	
+	private int frames = 0, maxFrames = 15, index = 0, maxIndex = 1;
+	private BufferedImage[] sprites;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
-		super(x, y, width, height, sprite);
+		super(x, y, width, height, null);
+		sprites = new BufferedImage[2];
+		sprites[0] = Game.spritesheet.getSprite(112, 16, 16, 16);
+		sprites[1] = Game.spritesheet.getSprite(128, 16, 16, 16);
 	}
 
 	public void tick() {
@@ -36,10 +46,19 @@ public class Enemy extends Entity {
 			y-=speed;
 		}
 		
+		frames++;
+		if (frames == maxFrames) {
+			frames = 0;
+			index++;
+			if (index > maxIndex) {
+				index = 0;
+			}
+		}
+		
 	}
 	
 	public boolean isColidding(int xnext, int ynext) {
-		Rectangle enemyCurrent = new Rectangle(xnext, ynext, World.TILE_SIZE, World.TILE_SIZE);
+		Rectangle enemyCurrent = new Rectangle(xnext + maskx, ynext + masky, maskw, maskh);
 		
 		for (int i=0; i < Game.enemies.size(); i++) {
 			Enemy e = Game.enemies.get(i);
@@ -52,5 +71,12 @@ public class Enemy extends Entity {
 			}
 		}
 		return false;
+	}
+	
+	public void render(Graphics g) {
+		g.drawImage(sprites[index], this.getX() - Camera.x, this.getY()- Camera.y, null);
+//		super.render(g);
+//		g.setColor(Color.blue);
+//		g.fillRect(this.getX() + maskx - Camera.x, this.getY() + masky - Camera.y, maskw, maskh);
 	}
 }
